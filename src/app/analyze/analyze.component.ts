d282cebf90864d79bc797897af9582e2e88ea9c1
DerTyp7 <janis@tealfire.de>
2023-09-26 16:34:46 +0200

    add help texts

--- src/app/analyze/analyze.component.ts
+++ src/app/analyze/analyze.component.ts
@@ -36,24 +36,24 @@ export class AnalyzeComponent implements OnInit {
     let minLat: number = parseFloat(this.lat!) - 0.13;
     let maxLat: number = parseFloat(this.lat!) + 0.13;
 
-    this.status = "Downloading OSM data...";
+    this.status = "Downloading OSM data... This might take a few minutes ";
 
     this.overpassApiService.sendQueryRequest(`${minLon},${minLat},${maxLon},${maxLat}`)
-    .pipe(catchError(err => {
-      console.error(err);
-      this.status = "<label style='color:rgb(209, 39, 39); font-weight:bold;'>Dowloading OSM data failed!</label>\
+      .pipe(catchError(err => {
+        console.error(err);
+        this.status = "<label style='color:rgb(209, 39, 39); font-weight:bold;'>Dowloading OSM data failed!</label>\
       <br>You may have been set on pause by the server, because of too many requests in a short amount of time.";
-      return of("");
-    }))
-    .subscribe((data: string) => {
-      if(data != ""){
-        let osm: OSM = this.osmService.parseOsmContent(data);
-        // Analyze the OSM data
-        this.status = "Analyze OSM data...";
-        this.osmAnalysis = this.osmAnalysisService.getOsmAnalysis(osm);
-        this.status = "";
-      }
-    });
+        return of("");
+      }))
+      .subscribe((data: string) => {
+        if (data != "") {
+          let osm: OSM = this.osmService.parseOsmContent(data);
+          // Analyze the OSM data
+          this.status = "Analyze OSM data...";
+          this.osmAnalysis = this.osmAnalysisService.getOsmAnalysis(osm);
+          this.status = "";
+        }
+      });
   }
 
 }
